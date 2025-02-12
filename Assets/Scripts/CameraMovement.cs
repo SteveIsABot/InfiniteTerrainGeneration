047d0f0b9ed61379af3b3f2c19267e19e435bc09
SteveIsABot <133403738+SteveIsABot@users.noreply.github.com>
2025-02-12 01:03:27 +0000

    Fully working camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] float mainSpeed = 100.0f;
-    [SerializeField] float shiftAdd = 250.0f;
-    [SerializeField] float maxShift = 1000.0f;
-    [SerializeField] float camSens = 0.25f;
+    [SerializeField] float mainSpeed = 5f;
+    [SerializeField] float speedBooster = 1.0f;
+    [SerializeField] float maxSpeed = 10.0f;
+    [SerializeField] float camSens = 0.5f;
+
+    private float yaw = 0;
+    private float pitch = 0;
+    private float currentSpeed = 5.0f;
+
+    void Start() {
+        currentSpeed = mainSpeed;
+    }
     void Update() {
 
-        if(Input.GetKey (KeyCode.W)) { transform.position += transform.forward.normalized * mainSpeed * Time.fixedDeltaTime; }
-        if(Input.GetKey (KeyCode.A)) { transform.position -= transform.right.normalized * mainSpeed * Time.fixedDeltaTime; }
-        if(Input.GetKey (KeyCode.S)) { transform.position -= transform.forward.normalized * mainSpeed * Time.fixedDeltaTime; }
-        if(Input.GetKey (KeyCode.D)) { transform.position += transform.right.normalized * mainSpeed * Time.fixedDeltaTime; }
+        pitch += Input.GetAxis("Mouse X") * camSens * 5.0f;
+        yaw -= Input.GetAxis("Mouse Y") * camSens * 5.0f;
+        currentSpeed = Input.GetKey(KeyCode.LeftControl) ? currentSpeed += speedBooster : currentSpeed = mainSpeed;
+
+        yaw = Mathf.Clamp(yaw, -85.0f, 85.0f);
+        currentSpeed = Mathf.Clamp(currentSpeed, mainSpeed, maxSpeed);
+
+        transform.eulerAngles = new Vector3(yaw, pitch, 0);
+        
+        Vector3 camFwd = transform.forward;
+        Vector3 camRight = transform.right;
+        camFwd.y = 0.0f;
+        camRight.y = 0.0f;
+        camFwd.Normalize();
+        camRight.Normalize();
+
+        if(Input.GetKey(KeyCode.W)) { transform.position += camFwd * currentSpeed * Time.fixedDeltaTime; }
+        if(Input.GetKey(KeyCode.A)) { transform.position -= camRight * currentSpeed * Time.fixedDeltaTime; }
+        if(Input.GetKey(KeyCode.S)) { transform.position -= camFwd * currentSpeed * Time.fixedDeltaTime; }
+        if(Input.GetKey(KeyCode.D)) { transform.position += camRight * currentSpeed * Time.fixedDeltaTime; }
+
+        if(Input.GetKey(KeyCode.Space)) { transform.position += Vector3.up * currentSpeed * 0.5f * Time.fixedDeltaTime; }
+        if(Input.GetKey(KeyCode.LeftShift)) { transform.position -= Vector3.up * currentSpeed * 0.5f * Time.fixedDeltaTime; }
     }
 }
